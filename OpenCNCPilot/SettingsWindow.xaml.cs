ef7f46822442631168fbba6b92dab94b3b8b014d
martin2250 <martin.225@web.de>
2016-02-03 14:32:14 +0100

    Basic UI

--- OpenCNCPilot/SettingsWindow.xaml.cs
+++ OpenCNCPilot/SettingsWindow.xaml.cs
@@ -10,32 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace OpenCNCPilot
 {
 	/// <summary>
-	/// Interaktionslogik für MainWindow.xaml
+	/// Interaktionslogik für SettingsWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class SettingsWindow : Window
 	{
-		SettingsWindow SettingsWindow = new SettingsWindow();
-
-		public MainWindow()
+		public SettingsWindow()
 		{
 			InitializeComponent();
-		}
-
-		private void Window_Closed(object sender, EventArgs e)
-		{
-			Properties.Settings.Default.Save();
-			SettingsWindow.Close();
-		}
-
-		private void Settings_Click(object sender, RoutedEventArgs e)
-		{
-			SettingsWindow.Show();
 		}
 	}
 }
